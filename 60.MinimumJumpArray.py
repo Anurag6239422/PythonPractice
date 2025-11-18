532269a63cb1023ec32b0adbb883c62f5e4a8a1b
Anurag6239422 <anuragsandilya22678@outlook.com>
2025-11-19 01:15:57 +0530

    19-11-2025 Done Minimum Jump Program

--- 60.MinimumJumpArray.py
+++ 60.MinimumJumpArray.py
@@ -1,24 +1,62 @@
 #450 DSA
 #Minimum Jump of Array
 
-arr = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]
+number = int(input("Enter how many element you want to enter : "))
+list1 = []
 
-first = 0
-jump = 0
+for i in range (0, number) :
+    no = int(input(f"Enter {i+1} : "))
+    list1.append(no)
+'''
+This is the best code but does not pass multiple test case
 
-while True :
-    if arr[first] == 0 :
+next_value = 0
+step = 0
+count = 0
+updated_index = 0
+
+while next_value < len(list1) :
+    if next_value == len(list1) - 1:
         break
-    if arr[first] > len(arr[first : ]) :
-        if arr[first] == arr[len(arr)-1] :
-            break
-        else :
-            jump += 1
-    else :
-        jump += 1
-        first += arr[first]
+    
+    step = list1[next_value]
 
-if jump == 0 :
+    if step <= 0 :
+        print(f"Sorry, Unable to jump : {count}")
+
+    updated_index = step + next_value
+    next_value = updated_index
+    count += 1
+
+print("Total Count :", count)
+
+
+'''
+
+n = len(list1)
+
+if n <= 1 :
+    print(0)
+
+if list[0] == 0 :
     print(-1)
-else :
-    print(f"Minimum Jump is : {jump}")
+
+step = list1[0]
+updated_index = list1[0]
+count = 1
+
+for i in range (1, n):
+    if i == n-1 :
+        print("Minimum Count Jump : ", count)
+
+    updated_index = max(updated_index, i+list1[i])
+
+    step -= 1
+
+    if step == 0 :
+        count += 1
+        if i >= updated_index :
+            print("Minimum Jump : -1")
+
+        step = updated_index - i
+        
